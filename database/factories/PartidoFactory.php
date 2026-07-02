8a2b30d3078cea61ffe2c2b290f161201b19573b
Limberth Cordova <cordovalimberth772@gmail.com>
2026-07-02 10:56:34 -0400

    actualizar

--- database/factories/PartidoFactory.php
+++ database/factories/PartidoFactory.php
@@ -11,49 +11,43 @@ class PartidoFactory extends Factory
     public function definition(): array
     {
         return [
-
             'torneo_id' => Torneo::factory(),
 
-            'equipo_local' => fake()->randomElement([
+            'equipo_local' => $this->faker->randomElement([
                 'Real Madrid',
                 'Barcelona',
                 'Liverpool',
                 'Manchester City',
-                        'equipo_local' => $this->faker->randomElement([
                 'Bayern Munich',
                 'Milan',
                 'Juventus'
             ]),
 
-            'equipo_visitante' => fake()->randomElement([
+            'equipo_visitante' => $this->faker->randomElement([
                 'Chelsea',
                 'Arsenal',
                 'Inter',
-                        'equipo_visitante' => $this->faker->randomElement([
                 'Napoli',
                 'Sevilla',
                 'Benfica',
                 'Porto'
             ]),
 
-            'fecha' => fake()->date(),
+            'fecha' => $this->faker->date(),
 
-            'hora' => fake()->time('H:i:s'),
-                        'fecha' => $this->faker->date(),
-                        'hora' => $this->faker->time('H:i:s'),
-                        'lugar' => $this->faker->city(),
-                        'estado' => $this->faker->randomElement([
+            'hora' => $this->faker->time('H:i:s'),
+
+            'lugar' => $this->faker->city(),
+
+            'estado' => $this->faker->randomElement([
                 'programado',
                 'en_juego',
                 'finalizado'
             ]),
 
-                        'marcador_local' => $this->faker->numberBetween(0, 5),
-                        'marcador_visitante' => $this->faker->numberBetween(0, 5),
-            'marcador_local' => fake()->numberBetween(0, 5),
+            'marcador_local' => $this->faker->numberBetween(0, 5),
 
-            'marcador_visitante' => fake()->numberBetween(0, 5),
-
+            'marcador_visitante' => $this->faker->numberBetween(0, 5),
         ];
     }
 }
